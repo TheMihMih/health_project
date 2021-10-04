901b4a036eef69dc388d5aedcd5037512946471d
IvanPozd <johndillon28cb@gmail.com>
2021-10-04 19:08:26 +0300

    ivan's changes

---
+++ webapp/__init__.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, flash, redirect, url_for
+from webapp.config import SECRET_KEY
+from webapp.model import db, User
+from webapp.forms import LoginForm
+from flask_login import LoginManager, login_user, logout_user, current_user
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    app.secret_key = SECRET_KEY
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    @app.route('/')
+    def index():
+        page_title = "Главная страница"
+        text = """Здесь будет какой-то текст! """
+        text2 = """Здесь будет еще какой-то текст! """
+        return render_template(
+            'index.html', page_title=page_title, text=text, text2=text2
+        )
+        
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated():
+            return redirect(url_for('index'))
+        page_title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=page_title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            email = User.query.filter_by(email=form.email.data).first()
+            if user and user.check_password(form.password.data) and email:
+                login_user(user)
+                flash("Вы успешно вошли на сайт")
+                return redirect(url_for('index'))
+            
+        flash("Неправильные имя или пароль")
+        return redirect(url_for('login'))
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash("Вы успешно вышли с сайта")
+        return redirect(url_for('index'))
+
+    return app
---
+++ webapp/model.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    def __repr__(self):
+        return f"<User name - {self.username}, User ID = {self.id}>"
