33fa4749a2633b307ca69dd9819e002f5be7bfca
IvanPozd <johndillon28cb@gmail.com>
2021-10-15 23:19:57 +0300

    refactoring news views.py

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -11,22 +11,20 @@ def index():
     page_title = "Главная страница"
     text = """Мы рады Вас приветствовать на нашем сайте """
     text2 = """Здесь будет интересный блок """
-    user = current_user
-    return render_template("news/index.html", page_title=page_title, text=text, text2=text2, user=user)
+    return render_template("news/index.html", page_title=page_title, text=text, text2=text2, user=current_user)
 
 
 @blueprint.route("/about")
 def about():
     page_title = "Наш проект"
-    user = current_user
-    return render_template("news/about.html", page_title=page_title, user=user)
+    return render_template("news/about.html", page_title=page_title, user=current_user)
 
 
 @blueprint.route("/news")
 def display_news():
     title = "Новости Python"
     news_list = BDConnector.query.order_by(BDConnector.id.desc()).all()
-    return render_template("news/news.html", page_title=title, news_list=news_list)
+    return render_template("news/news.html", page_title=title, news_list=news_list, user=current_user)
 
 
 @blueprint.route("/news/<int:news_id>", methods=["GET"])
@@ -34,5 +32,5 @@ def news(news_id):
     news_context = BDConnector.query.filter(BDConnector.id == news_id).first()
     page_title = news_context.title
     return render_template(
-        "news/news_id.html", page_title=page_title, news_context=news_context
+        "news/news_id.html", page_title=page_title, news_context=news_context, user=current_user
     )
