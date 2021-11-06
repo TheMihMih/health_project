4c53b4336a93d1986a03490e15056e22de51465b
IvanPozd <johndillon28cb@gmail.com>
2021-11-06 22:33:09 +0300

    made parsers for news

---
+++ get_news.py
@@ -0,0 +1,8 @@
+from webapp import create_app
+from webapp.news.parser import zoj
+
+app = create_app()
+with app.app_context():
+    #zoj.get_snippets()
+    zoj.get_meal_news()
+    zoj.get_train_news()
--- webapp/news/parser/zoj.py
+++ webapp/news/parser/zoj.py
@@ -1,20 +1,54 @@
-from datetime import date, datetime
+import platform
+import locale
 
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
-
 from webapp.news.parser.utils import get_html, save_news
 
-def get_snippets():
-    html = get_html('https://food.ru/search?query=%D0%B7%D0%BE%D0%B6&material=article')
-    if html:
-        soup = BeautifulSoup(html, 'html.parser')
-        all_news = soup.find_all('div', class_='grid_container__1uSKI')
-        for news in all_news:
-            #title = news.find('div', class_='')
-            url = news.find('a')
-            if url != None:
-                print(url)
-            #text = news.find('div', class_='markdown_p__1f4Us').text
-            #category="Питание"
+if platform.system() == "Windows":
+    locale.setlocale(locale.LC_ALL, "russian")
+else:
+    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 
-            #save_news(title, text, category, url)
+
+def parse_date(date_str):
+    if "сегодня" in date_str:
+        today = datetime.now()
+        date_str = date_str.replace("сегодня", today.strftime('%d %B %Y'))
+    elif "вчера" in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace("вчера", yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%H:%M %d %B %Y')
+    except ValueError:
+        return datetime.now()
+
+
+def get_meal_news():
+    html = get_html('https://tolknews.ru/search?q=%D0%9F%D0%B8%D1%82%D0%B0%D0%BD%D0%B8%D0%B5')
+    soup = BeautifulSoup(html, 'html.parser')
+    if html:
+        all_news = soup.find_all('div', class_='news--simple')
+        for news in all_news:
+            title = news.find('a')['title']
+            #text = news.find('div', class_='news__content').find('a').text
+            published = news.find('time').text
+            published = parse_date(published)
+            category = "Питание"
+            url = (f"https://tolknews.ru/{news.find('a')['href']}")
+            save_news(title, published, category, url)
+
+
+def get_train_news():
+    html = get_html('https://tolknews.ru/search?q=%D0%A2%D1%80%D0%B5%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B8')
+    soup = BeautifulSoup(html, 'html.parser')
+    if html:
+        all_news = soup.find_all('div', class_='news--simple')
+        for news in all_news:
+            title = news.find('a')['title']
+            #text = news.find('div', class_='news__content').find('a').text
+            published = news.find('time').text
+            published = parse_date(published)
+            category = "Тренировки"
+            url = (f"https://tolknews.ru/{news.find('a')['href']}")
+            save_news(title, published, category, url)
